707bd49744adb1c33e5442513f30bc86c53bfe88
d3fau1t <aciddust20@gmail.com>
2021-10-11 15:47:44 +0900

    :seedling: Init next
    now we begin

---
+++ vinyl-web/pages/index.tsx
@@ -0,0 +1,59 @@
+import type { NextPage } from 'next'
+import Image from 'next/image'
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { vinylUrl } from '../env'
+import { CardView } from '../components/card'
+// import './api/vinyl'
+
+const Home: NextPage = () => {
+  useEffect(() => {
+    getVinylInfo()
+    // renderVinyl()
+  }, []) // TODO: page end or not flag 로 값 바뀔때마다 불러오는건어떨까.
+  const [vinylInfoForPage, setVinylInfoForPage] = useState<JSON | undefined>(undefined)
+
+  const getVinylInfo = async () => {
+    if (!vinylUrl) {
+      console.log('not vinyl url')
+      return
+    }
+    const vinyls = await fetch(vinylUrl, {
+      method: 'GET',
+    })
+    const vinylInfo = await vinyls.json()
+    setVinylInfoForPage(vinylInfo)
+  } 
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Vinyl Trends</title>
+        <meta name="description" content="Trends of Japanese vinyl store" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <div className='md:container md:mx-auto' style={{
+          backgroundColor: '#c4c4c4'
+        }}>
+          {/* <CardView /> */}
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://github.com/dev4hobby/amazon-japan-vinyl-alert"
+          target="#amazon-japan-vinyl-alert"
+          rel="noopener noreferrer"
+        >
+          d3fau1t: amazon-japan-vinyl-alert
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default Home
